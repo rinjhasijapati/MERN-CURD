d7aca3df56528f8f26c5eac6bc4f9b262526a65d
rinjhasijapati <aastha_sijapati@yahoo.com>
2024-03-30 21:07:41 +0545

    fetch record and display

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Users() {
-  const [users, setUsers] = useState([{
-    Name: "person", Email: "person@gmail.com", Age: 21, 
-  }]);
+  const [users, setUsers] = useState([]);
   
+  useEffect(() => {
+    axios.get('/http://localhost:3001')
+    .then(result => setUsers(result.data))
+    .catch(err => console.log(err))
+  }, [])
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
       <div className='w-50 bg-white rounded p-3'>
@@ -23,11 +27,11 @@ function Users() {
             {
               users.map((user) => {
                 return <tr>
-                  <td>{user.Name}</td>
-                  <td>{user.Email}</td>
-                  <td>{user.Age}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.age}</td>
                   <td>
-                    <Link to="/update" className='btn btn-success'>Update</Link> <button>Delete</button>
+                    <Link to="/update" className='btn btn-success'>Update</Link> <button className='btn btn-danger'>Delete</button>
                   </td>
                 </tr>
               })
